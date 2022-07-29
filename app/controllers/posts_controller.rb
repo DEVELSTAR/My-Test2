b20003ce617665643dc8023a2574f91ca7ca751c
Mohd Akibuddin <akeebudeen786@gmail.com>
2022-07-29 18:26:53 +0530

    user_id migration, modify post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:edit, :update, :destroy, :index, :show]
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    #@created_by = Post.current_user.name
+    @posts = Post.all.order('created_at DESC')
   end
 
   # GET /posts/1 or /posts/1.json
@@ -13,6 +15,7 @@ class PostsController < ApplicationController
   # GET /posts/new
   def new
     @post = Post.new
+    #@post = current_user.posts.build
   end
 
   # GET /posts/1/edit
@@ -21,7 +24,9 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
+    #@post = current_user.posts.build(post_params)
     @post = Post.new(post_params)
+    @post.user = current_user
 
     respond_to do |format|
       if @post.save
