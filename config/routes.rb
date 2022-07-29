ccf382b7b898cda4d7b100cea2b262df00843810
Mohd Akibuddin <akeebudeen786@gmail.com>
2022-07-29 11:45:04 +0530

    scaffold post, better_error

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   devise_for :users, controllers: { registrations: 'registrations' }
   root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
